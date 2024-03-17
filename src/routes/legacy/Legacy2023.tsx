c4c2e4e40ce97ca8d2b4ecd23c52672c828e6097
ramondeleonca <ramondeleonca@gmail.com>
2024-03-17 12:02:47 -0600

    Added legacy 2023 description

--- src/routes/legacy/Legacy2023.tsx
+++ src/routes/legacy/Legacy2023.tsx
@@ -5,7 +5,6 @@ export default function Legacy2023() {
             <h1 className="mb-4 text-7xl w-full text-center text-transparent bg-clip-text bg-gradient-to-t from-transparent to-75% to-text">MAFAVITO</h1>
 
             <div className="gallery">
-                <img src="/IMG_2356.JPG" alt="" loading="lazy"></img>
                 <img src="/IMG_2359.JPG" alt="" loading="lazy"></img>
                 <img src="/IMG_2378.JPG" alt="" loading="lazy"></img>
                 <img src="/IMG_2399.JPG" alt="" loading="lazy"></img>
@@ -25,7 +24,27 @@ export default function Legacy2023() {
 
             <div id="about">
                 <h3 className="my-4 font-neutro font-bold text-4xl w-full text-center">About MAFAVITO</h3>
-                <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/0zpflsYc4PA?si=hM1_z0W7yuawxS-N" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                <div className="w-full h-full flex flex-wrap justify-evenly items-center p-4 my-8">
+                    <div className="p-10 w-2/5 max-md:w-full">
+                        <div className="text-xl mb-4">
+                            <p><b>[NAME: MAFAVITO]</b></p>
+                            <p><b>[STATUS: RETIRED]</b></p>
+                            <p><b>[WEIGHT: 56KG]</b></p>
+                            <p><b>[SIZE: 35" x 45"]</b></p>
+                        </div>
+                        
+                        <p className="text-xl">MAFAVITO had multiple iterations, however this is its last one, it was built using an andymark KitBot 4 CIM motor tank drivetrain with a 4 CIM motor pulley system to lower and raise the arm's angle. The grabber is powered by a single REV NEO motor with a limit switch to detect wether it has a piece or not. At the begining of the season Mafavito looked a little rough, however torwards the end, the team learned a lot and were able to fix him up and keep on competing!</p>
+                    </div>
+
+                    <div className="flex items-center justify-center h-full my-4">
+                        <img src="/IMG_2399.JPG" alt="" loading="lazy" className="object-cover w-96 h-full aspect-square"></img>
+                    </div>
+
+                    <div>
+                        <h4 className="text-center m-2 ">The challenge</h4>
+                        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/0zpflsYc4PA?si=hM1_z0W7yuawxS-N" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                    </div>
+                </div>
             </div>
         </>
     )
